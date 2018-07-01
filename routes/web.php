5e60501dc027c1b88e19c85c74997011def43040
Bit Art <courses@bitart.com.br>
2018-07-01 08:15:32 -0300

    My first Branch

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('notifications',['PUSHER_APP_KEY'=> env('PUSHER_APP_KEY'), 'PUSHER_APP_CLUSTER' => env('PUSHER_APP_CLUSTER'), 'PUSHER_APP_SECRET' => env('PUSHER_APP_SECRET')]);
+});
+Route::get('test', function () {
+    event(new notify\Events\StatusLiked('Here comes my message', 'Fabiano Viana'));
+    return "Event has been sent!";
+});
